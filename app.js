55fb7386672ffb0136c15ce8de3792717a0210ad
이호준 <juns9050@gmail.com>
2021-08-02 18:32:43 +0900

    2021.08.02 css, 채팅 기능, 스크롤링

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express(); // express 를 실행할 내용을 app에 담음
 const path = require("path") // path 모듈을 불러옴, url을 쉽게 만들 수 있음
 const server = http.createServer(app) // express로 구현한 app을 http를 통해서 실행될 수 있도록 함 
 const socketIO = require("socket.io") // socket.io를 변수에 담음
+const moment = require("moment")
 
 const io = socketIO(server); // 서버에 담음
 
@@ -13,8 +14,12 @@ const PORT = process.env.PORT || 5000; // 5000번 포트를 사용
 
 io.on("connection", (socket) => { // connection이 이루어지면 연결에 대한 정보를 socket에 담음
     socket.on("chatting", (data) => {
-        console.log(data)
-        io.emit("chatting", data) // 서버에서 프론트로 보냄
+        const {name, msg} = data;
+        io.emit("chatting", {
+            name,
+            msg,
+            time: moment(new Date()).format("h:ss A")
+        }) // 서버에서 프론트로 보냄
     })
 })
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,21 +1,54 @@
 "use strict"
 const socket = io();
 
-const nickname = document.querySelector("#nickname") // dom 형태로 전송
-const chatList = document.querySelector(".chatting-list")
-const chatInput = document.querySelector(".chatting-input")
-const sendButton = document.querySelector(".send-button")
+const nickname = document.querySelector("#nickname"); // dom 형태로 전송
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
 
-sendButton.addEventListener("click", ()=> {
+chatInput.addEventListener("keypress", (event) => {
+    if(event.keyCode === 13) {
+        send()
+    }
+})
+
+function send() {
     const param = { // 서버에 오브젝트 형태로 전송
         name: nickname.value,
         msg: chatInput.value
     }
     socket.emit("chatting", param)
-})
+    chatInput.value = ""; // 채팅 초기화
+}
+
+sendButton.addEventListener("click", send)
 
 socket.on("chatting", (data) => { // 서버에서 보낸 내용을 확인
     console.log(data)
+    const { name, msg, time } = data;
+    const item = new LiModel(name, msg, time); // LiModel 인스턴트화
+    item.makeLi()
+    displayContainer.scrollTo(0, displayContainer.scrollHeight) // 스크롤 생길 시 포커싱
 })
+
+function LiModel(name, msg, time) {
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = () => {
+        const li = document.createElement("li"); // li 태그 생성
+        li.classList.add(nickname.value === this.name ? "sent": "received") //nickname value가 서버에 넘겨받은 이름과 같으면 클래스를 sent, 아니라면 received
+        const dom =`<span class="profile">
+                    <span class="user">${this.name}</span>
+                    <img class="image" src="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/7r5X/image/9djEiPBPMLu_IvCYyvRPwmZkM1g.jpg" alt="any">
+                    </span>
+                    <span class="message">${this.msg}</span>
+                    <span class="time">${this.time}</span>`;
+    li.innerHTML = dom;
+    chatList.appendChild(li)
+    }
+}
 
 console.log(socket)
